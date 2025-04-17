29f8a46fc31354837fcb11ec708827811037af80
A-stone0161 <annabels0161@gmail.com>
2025-04-17 12:45:01 -0700

    Edits

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - pseudo code for lab4
+// Author: Annabelle Stone
+// Date: 04/17/2025
 
 // Constants
 
@@ -15,7 +15,17 @@ function myFunction(param1, param2) {
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
+  //print hangman platform
+//generate a random word from a predefined list
+      //print dashes for the number of letters in the word
+//accept user guesses of letters
+  //if correct, print letter in correct space
+  //if not correct, print body parts in order: head, body, left arm, right arm, left leg, right leg, game over
+//if game over, restart and generate a new word
+//if user guesses word within six guesses, print “Congratulations!” and full body walking away
 }
 
 // let's get this party started
 main();
+
+

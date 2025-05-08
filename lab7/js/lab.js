b04e98f59a8b204793e1aea3ed027783cd18b663
A-stone0161 <annabels0161@gmail.com>
2025-05-07 18:05:30 -0700

    Edits

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -17,18 +17,20 @@
 function shuffleArray(array) {
     let currentIndex = array.length, temporaryValue, randomIndex;
 
-
-    // Sorts while there are still elements to shuffle  
+// While the index still has elements to shuffle...   
     while (0 !== currentIndex) {
       let randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
- 
-      // Swaps the current element with a random one    
+
      temporaryValue = array[currentIndex];
      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
       }
+// Return the now shuffled array
       return array;
     }
+
+// Switch the title case so that it can be used and printed on the page
+    // Credit: https://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript
     function toTitleCase(str) {
         return str.replace(
             /\w\S*/g,
@@ -38,11 +40,12 @@ function shuffleArray(array) {
         );
      }
    
+// Sort the userName alphabetically 
     function sortUserName(userName) {
         return userName.toLowerCase().split("").sort().join("");
     }
 
-
+// Swap from array to string and randomize the name
     function nameRandomizer(userName) {
         var nameArray = userName.toLowerCase().split("");      
         console.log("nameArray =", nameArray);
@@ -55,7 +58,7 @@ function shuffleArray(array) {
 
 
 function printName() {
-   
+   // Credit to Wes Modes for the window.prompt() function https://wmodes.github.io/art101/lab7/js/lab7.js
     var userName = window.prompt("Hi, may I please have your name?");
     document.writeln("<style>:root {---textlen: " + userName.length + ";}</style>");
    
@@ -70,11 +73,6 @@ function printName() {
 // Call the function
 printName();
 
-
-// Variables
-
-
-// Body
 
 
 
--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,10 +1,49 @@
-// index.js - Lab 7
+// index.js - Lab 8
 // Author: Annabelle Stone 
-// Date: 04/30/2025
+// Date: 05/08/2025
 
-// Constants
 
-// Functions
 
-// Variables
+// Define the array.
+const numArray = [1, 3, 5, 7, 9, 20, 24, 26];
 
+console.log("This is our starting array: " + numArray);
+
+// This function gives us the square root of the inpur number
+function squareNumber(num) {
+  return num * num;
+}
+
+
+console.log("These are the square roots: " + numArray.map(squareNumber));
+
+
+// This anonymous function returns the array of 
+const dividedByPointFive = numArray.map(function(num) {
+  return num / .5;
+});
+
+console.log("What if we divided the numbers by .5... " + dividedByPointFive);
+
+//6.
+
+
+const getOddNumbers = function(numbers) {
+  return numbers.filter(function(num) {
+    return num % 2 !== 0;
+  });
+};
+
+console.log("What if we pulled out just the odd numbers?");
+
+  setTimeout(function(){
+    const result = getOddNumbers(numArray);
+    console.log("Sorry that took a second (or three). Here's the odd numbers: " + result);
+  }, 3000);
+
+ var mapResults = "Check the console for the full results, but here's a sneak peek: <br>" + ":) <br>";
+
+  $("#output").html(mapResults + dividedByPointFive.join("<br>"));
+
+
+ 

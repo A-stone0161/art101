d4aad686aaba0e5db4086b1675c8b4f784405411
A-stone0161 <annabels0161@gmail.com>
2025-05-27 12:02:09 -0700

    Fizzbuzzboom

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -3,13 +3,21 @@
 // Date: 05/25/2025
 //Loops
 
-
+// Fizzbuzzboom function
 function fizzBuzzBoom() {
   let oneLongString = "";
 
   for (let i = 1; i <= 200; i++) {
     let str = "";
-
+// If else statements using modular division to determine the output of Fizz, Buzz, Boom, or FizzBuzzBoom
+    // If i is divisible by 3, 5, and 7, output "FizzBuzzBoom!"
+    // If i is divisible by 3 and 5, output "FizzBuzz!"
+    // If i is divisible by 3 and 7, output "FizzBoom!"
+    // If i is divisible by 5 and 7, output "BuzzBoom!"
+    // If i is divisible by 3, output "Fizz!"
+    // If i is divisible by 5, output "Buzz!"
+    // If i is divisible by 7, output "Boom!"
+    // Otherwise, output the number itself
     if (i % 3 === 0 && i % 5 === 0 && i % 7 === 0) {
       str = "FizzBuzzBoom!";
     } else if (i % 3 === 0 && i % 5 === 0) {
@@ -27,11 +35,10 @@ function fizzBuzzBoom() {
     } else {
       str = i;
     }
-
     oneLongString += i + " " + str + "<br>";
   }
-
+// String output with id "output"
   $("#output").html(oneLongString);
 }
-
+// Call the fizzBuzzBoom function to execute it
 fizzBuzzBoom();
